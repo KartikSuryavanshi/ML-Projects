c5c969cc0d26377455670a45b1d33a544476bd0c
Kartik Suryavanshi <158498247+KartikSuryavanshi@users.noreply.github.com>
2025-12-10 10:06:53 +0530

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -18,24 +19,34 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        for i in range(len(models)):
+        for i in range(len(list(models))):
             model = list(models.values())[i]
-            model_name = list(models.keys())[i]
-            logging.info(f"Training the model: {model_name}")
-            model.fit(X_train, y_train)
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
 
             y_test_pred = model.predict(X_test)
 
-            r2_square = r2_score(y_test, y_test_pred)
+            train_model_score = r2_score(y_train, y_train_pred)
 
-            report[model_name] = r2_square
-            logging.info(f"{model_name} R2 Score: {r2_square}")
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
+    
 
     except Exception as e:
         raise CustomException(e, sys)
